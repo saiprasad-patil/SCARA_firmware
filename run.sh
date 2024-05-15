61fbd452a6505dbe9022bc81c09a37474eb82638
Saiprasad Patil <saiprasad30042003@gmail.com>
2024-05-16 02:31:24 +0530

    Updated states

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ros2 run robot_state_publisher robot_state_publisher --ros-args -p robot_description:="$(xacro ~/fyp/v2scara/src/description/robot.urdf.xacro)" &
-
+sleep 2
 ros2 run joint_state_publisher_gui joint_state_publisher_gui &
 
-rviz2
+rviz2 --f base_link
--- v2scara/src/state_publisher/state_publisher/publisher.py
+++ v2scara/src/state_publisher/state_publisher/publisher.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import rclpy
+import socket
 from rclpy.node import Node
 from sensor_msgs.msg import JointState     
-     
+
+HOST = '192.168.241.55'
+PORT = 12345
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
 class PublisherNode(Node): 
         def __init__(self):
             super().__init__("publisher") 
             self.subscriber = self.create_subscription(JointState,"joint_states",self.callback_publisher, 10)
             self.get_logger().info("Publisher started")
         def callback_publisher(self, msg):
-            print(msg.position)
+            value = [[str(msg.name[0]),str(msg.position[0])],[str(msg.name[1]),str(msg.position[1])],[str(msg.name[2]),str(msg.position[2])]]
+            print(value)
+            client_socket.send(str(value).encode('utf-8'))
      
 def main(args=None):
         rclpy.init(args=args)
