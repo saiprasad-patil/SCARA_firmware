8e2ee8a3e77631bbf9faa9f22b3f6bbfa31f61ee
Saiprasad Patil <saiprasad30042003@gmail.com>
2024-05-18 07:31:39 +0530

    1

--- v2scara/src/state_publisher/state_publisher/publisher.py
+++ v2scara/src/state_publisher/state_publisher/publisher.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import rclpy
 import socket
+import math
 from rclpy.node import Node
 from sensor_msgs.msg import JointState     
 
-HOST = '192.168.241.55'
+HOST = '192.168.222.55'
 PORT = 12345
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
@@ -14,9 +15,9 @@ class PublisherNode(Node):
             self.subscriber = self.create_subscription(JointState,"joint_states",self.callback_publisher, 10)
             self.get_logger().info("Publisher started")
         def callback_publisher(self, msg):
-            value = [[str(msg.name[0]),str(msg.position[0])],[str(msg.name[1]),str(msg.position[1])],[str(msg.name[2]),str(msg.position[2])]]
+            value = msg.name[0]+str(int(10-(msg.position[0]*-1)*10/0.445))+"#"+msg.name[1]+str(int((msg.position[1]*180/math.pi)+90))+"#"+msg.name[2]+str(int((msg.position[2]*180/math.pi)+90))+"#"+msg.name[3]+str(int((msg.position[3]*180/math.pi)+90))
             print(value)
-            client_socket.send(str(value).encode('utf-8'))
+            client_socket.send(str(value).encode("utf-8"))
      
 def main(args=None):
         rclpy.init(args=args)
